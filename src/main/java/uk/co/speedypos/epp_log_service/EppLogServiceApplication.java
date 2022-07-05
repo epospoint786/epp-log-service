bd2f32e1e5e5a52258d59b193b7ca8fa12985665
Supto <codersupto@gmail.com>
2022-07-05 19:21:47 +0600

    Initial commit by Supto Purakayasto

---
+++ src/main/java/uk/co/speedypos/epp_log_service/EppLogServiceApplication.java
@@ -0,0 +1,13 @@
+package uk.co.speedypos.epp_log_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EppLogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EppLogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/speedypos/epp_log_service/EppLogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.speedypos.epp_log_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EppLogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
